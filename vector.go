9ce9c82ddc50000e4ada326dd9d54eab5101d6e6
Slo W. Poke <proxypoke@lavabit.com>
2012-06-27 09:16:47 +0000

    We Anti-License now.

--- vector.go
+++ vector.go
@@ -1,10 +1,8 @@
-// Copywrong 2012 slowpoke <proxypoke at lavabit dot com>
-// Repository: https://github.com/proxypoke/vector.go
+// Author: slowpoke <proxypoke at lavabit dot com>
+// Repository: https://gist.github.com/proxypoke/vector
 //
-// This program is free software under the terms of the
-// Do What The Fuck You Want To Public License v2,
-// which can be found in a file called COPYING included
-// with this program or at http://sam.zoy.org/wtfpl/COPYING
+// This program is free software under the non-terms
+// of the Anti-License. Do whatever the fuck you want.
 
 // Package vector implements mathematical vectors over float64 values, with
 // common operations defined on them, like addition, the scalar product or
